2122780ec16cd08c6ede54ac29ca46be95e31be7
Cody Dracars <cdracars@Codys-MacBook-Pro.local>
2014-06-19 16:37:08 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+/* global app:true */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+  var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ]);
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+  app.constant('FIREBASE_URL', 'https://sweltering-fire-7306.firebaseio.com/');
